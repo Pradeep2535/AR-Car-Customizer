8dd7abde27d8b31c8c3cdacbe40b94b2800ecf82
Pradeep S <117376703+Pradeep2535@users.noreply.github.com>
2024-08-10 11:52:13 +0530

    commit

---
+++ Assets/Scripts/changecolor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changecolor : MonoBehaviour
+{
+    public Color[] colors;
+    public Renderer[] mats;
+
+    public void Red(){
+
+        for(int i=0;i<mats.Length;i++){
+            mats[i].material.color=colors[0];
+        }
+    }
+
+    public void Navy(){
+        for(int i=0;i<mats.Length;i++){
+            mats[i].material.color=colors[1];
+        }
+    }
+
+    public void Yellow(){
+        for(int i=0;i<mats.Length;i++){
+            mats[i].material.color=colors[2];
+        }
+    }
+
+    public void Green(){
+        for(int i=0;i<mats.Length;i++){
+            mats[i].material.color=colors[3];
+        }
+    }
+
+    public void Blue(){
+        for(int i=0;i<mats.Length;i++){
+            mats[i].material.color=colors[4];
+        }
+    }
+}
